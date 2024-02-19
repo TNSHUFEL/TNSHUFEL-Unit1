8ac88b66a6b1835b0d4d8f2034106ca57c034ba4
Sura <60330226+TNSHUFEL@users.noreply.github.com>
2024-02-18 21:20:11 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const colours = ['Red', 'Blue', 'Green', 'Orange', 'Purple'];
+
+for (let i = 0; i < colours.length; i++) {
+    console.log(`${colours[i]} is my favorite color`);
+}
+
+const toTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+
+const userInput = prompt("Please Enter Your Email Address");
+
